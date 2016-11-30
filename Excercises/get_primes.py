c27ddc747d8baf81853db0fede3ca66559f98e3d
Sergio Nunez <Sergio.Nunez@rosslyn.local>
2016-11-30 18:40:41 +0000

    some prime number related stuff

--- Excercises/get_primes.py
+++ Excercises/get_primes.py
@@ -1,3 +1,4 @@
+import math
 
 def eratostheres_primes(n):
     primes = []
@@ -11,6 +12,29 @@ def eratostheres_primes(n):
                 i+=1
     return primes
 
+
+def SoE(n):
+    boolTable = [True for x in range(n-1)]
+    for x in xrange(2,n+1):
+        if boolTable[x-2]:
+            i = 2
+            while x*i <= n:
+                boolTable[x*i-2]=False
+                i+=1
+    return boolTable
+
+def SoE_segmented(n):
+    sqr = int(math.sqrt(n))
+
+def twin_primes(n,m):
+    boolTable = SoE(m)
+    print boolTable
+    res = 0
+    for i in range(len(boolTable)-2):
+        if i+2>=n and boolTable[i] and boolTable[i+2]:
+            res+=1
+    return res
+            
 
     
     
