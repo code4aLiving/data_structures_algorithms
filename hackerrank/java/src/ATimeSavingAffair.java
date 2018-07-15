1724d73d45624620579c2bcdd1e0113edaca0a25
snava10 <sergio.nava89@gmail.com>
2018-07-15 15:07:11 +0100

    ATimeSavingAffair

--- hackerrank/java/src/ATimeSavingAffair.java
+++ hackerrank/java/src/ATimeSavingAffair.java
@@ -1,89 +1,132 @@
+import java.io.IOException;
 import java.util.*;
 
 public class ATimeSavingAffair {
 
-    public static void main(String[] args) {
-
+    // Complete the leastTimeToInterview function below.
+    static long leastTimeToInterview(int n, int k, int m) {
+        // Return the least amount of time needed to reach the interview location in seconds.
+        Map<Integer, Set<Tuple<Integer, Long>>> graph = new HashMap<>();
+        for (int i = 0; i < m; i++) {
+            int u = scanner.nextInt();
+            int v = scanner.nextInt();
+            long t = scanner.nextLong();
+            if (!graph.containsKey(u)) graph.put(u, new HashSet<>());
+            if (!graph.containsKey(v)) graph.put(v, new HashSet<>());
+            graph.get(u).add(Tuple.build(v, t));
+            graph.get(v).add(Tuple.build(u, t));
+        }
+        long[] x = dijkstra(n * 2, 1, graph, k);
+        return x[n];
     }
 
-    static class Tuple implements Comparable<Tuple> {
-        int first, second;
-
-        Tuple(int first, int second) {
-            this.first = first;
-            this.second = second;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            Tuple other = (Tuple) o;
-            return first == other.first && second == other.second;
-        }
-
-        @Override
-        public int hashCode() {
-            return first & second;
-        }
-
-        @Override
-        public int compareTo(Tuple o) {
-            return Integer.compare(first, o.first);
-        }
-    }
-
-    static int[][] dijkstra(final int n, final int s, final Map<Integer, Set<Tuple>> graph) {
-        //initialize
-        final int[] distance = new int[n + 1];
-        final int[] prev = new int[n + 1];
+    static long[] dijkstra(final int n, final int s, final Map<Integer, Set<Tuple<Integer, Long>>> graph, int k) {
+        final long[] distance = new long[n + 1];
         final boolean[] visited = new boolean[n + 1];
-        for (int i = 1; i < n + 1; i++) {
-            distance[i] = i == s ? 0 : Integer.MAX_VALUE;
-            prev[i] = -1;
-        }
-        PriorityQueue<Tuple> queue = new PriorityQueue<>();
-        queue.add(new Tuple(distance[s], s));
 
-        while (queue.size() > 0) {
-            // Tuple du = getMinimum(setS);
-            Tuple du = queue.poll();
+        for (int i = 1; i < distance.length; i++) {
+            distance[i] = Long.MAX_VALUE;
+        }
+        distance[s] = 0l;
+
+        // Priority queue of tuples <Distance to origin, Vertex>
+        PriorityQueue<Tuple<Long, Integer>> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(Tuple.build(0l, s));
+
+        while (priorityQueue.size() > 0) {
+            Tuple<Long, Integer> du = priorityQueue.poll();
             int u = du.second;
+            if (visited[u]) continue;
+
+            //Once a vertex has been extracted from the queue it reached its optimum
             visited[u] = true;
-            for (Tuple vw : graph.get(u)) {
-                int v = vw.first;
-                if (visited[v])
-                    continue;
-                Tuple tv = new Tuple(distance[v], v);
-                // if (setS.contains(tv)) setS.remove(tv);
-                int weight_uv = vw.second;
-                if (relax(u, v, weight_uv, distance, prev)){
-                    // At this point we have to do decrease key of tv
-                    // queue.remove(tv); // This operation at the moment is O(n). It needs to be O(1) or O(log N)
-                    tv.first = distance[v];
-                    queue.add(tv); // This oi O(log N)
+
+            for (Tuple<Integer, Long> uv : graph.get(u)) {
+                int v = uv.first;
+                long weightuv = uv.second;
+                // In the graph each tuple is an edge <vertex, weight>
+                if (relax(u, v, weightuv, distance, k)) {
+                    //We could relax U ~> V
+                    // Add v back to the priority queue with the new updated distance.
+                    // It would be better to do decrease key but that's not available in java's priority queue
+                    priorityQueue.add(Tuple.build(distance[v], v));
                 }
-                //setS.add(tv);
             }
         }
-        return new int[][]{distance, prev};
+
+        return distance;
     }
 
-    static Tuple getMinimum(Set<Tuple> s) {
-        Tuple res = new Tuple(Integer.MAX_VALUE, -1);
-        for (Tuple t : s) {
-            if (res.first > t.first) {
-                res = t;
-            }
+    static boolean relax(int u, int v, long weightuv, long[] distance, int k) {
+        long timePenalty = 0;
+        long x = distance[u] / k;
+        if (x % 2 > 0) {
+            //The light is red
+            timePenalty = (x + 1) * k - distance[u];
         }
-        s.remove(res);
-        return res;
-    }
-
-    static boolean relax(int u, int v, int weightuv, int[] distance, int[] prev) {
-        if (distance[v] > distance[u] + weightuv) {
-            distance[v] = distance[u] + weightuv;
-            prev[v] = u;
+        if (distance[v] > distance[u] + weightuv + timePenalty) {
+            distance[v] = distance[u] + weightuv + timePenalty;
             return true;
         }
         return false;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int k = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int m = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = leastTimeToInterview(n, k, m);
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
+
+class Tuple<E extends Comparable<E>, F extends Comparable<F>> implements Comparable<Tuple<E, F>> {
+    final public E first;
+    final public F second;
+
+    public Tuple(E first, F second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public E getFirst() {
+        return first;
+    }
+
+    public F getSecond() {
+        return second;
+    }
+
+    public static <E extends Comparable<E>, F extends Comparable<F>> Tuple<E, F> build(E first, F second) {
+        return new Tuple<>(first, second);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Tuple other = (Tuple) o;
+        return first == other.first && second == other.second;
+    }
+
+    @Override
+    public int hashCode() {
+        return first.hashCode() & second.hashCode();
+    }
+
+    @Override
+    public int compareTo(Tuple<E, F> o) {
+        int compareFirst = first.compareTo(o.first);
+        return compareFirst == 0 ? second.compareTo(o.second) : compareFirst;
     }
 }
